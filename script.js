7b9995c8af361951e278140482625f1365ee3f6f
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-08-21 17:26:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    greet() {
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-    }
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
 }
+
+Person.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+};
+
 
 class Employee extends Person {
     constructor(name, age, jobTitle) {
